a7a747f4182e94e07366d63116d27fb32de906a1
ErickJamesBossing <aiahpagas@gmail.com>
2024-09-02 10:11:43 +0800

    Initial commit

---
+++ src/IT20_Erick1/main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package IT20_Erick1;
+
+/**
+ *
+ * @author Erick James Rubin
+ */
+public class main {
+        public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
